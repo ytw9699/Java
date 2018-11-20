29c01a8583b8a8d830a3d7e581e9e87b5a15b072
ytw9699 <ytw9699@gmail.com>
2018-11-20 15:46:57 +0900

    배열이용 최소값 최대값

--- java_jungsuk/src/ch05/ArrayEx06_최소값최대값.java
+++ java_jungsuk/src/ch05/ArrayEx06_최소값최대값.java
@@ -1,12 +1,13 @@
 package ch05;
-class ArrayEx06 { 
+class ArrayEx06_최소값최대값 { 
 	public static void main(String[] args) { 
+		
 		int[] score = { 79, 88, 91, 33, 100, 55, 95}; 
 
 		int max = score[0]; // 배열의 첫 번째 값으로 최대값을 초기화 한다. 
 		int min = score[0]; // 배열의 첫 번째 값으로 최소값을 초기화 한다. 
 
-		for(int i=1; i < score.length;i++) {
+		for(int i=1; i < score.length; i++) {
 			if(score[i] > max) { 
 				max = score[i]; 
 			} else if(score[i] < min) { 
