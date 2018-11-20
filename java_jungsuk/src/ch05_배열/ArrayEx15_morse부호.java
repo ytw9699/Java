6e7fbee83b544c70d6cc691f928e024c4ce9bccf
ytw9699 <ytw9699@gmail.com>
2018-11-20 22:24:56 +0900

    패키지이름변경

---
+++ java_jungsuk/src/ch05_배열/ArrayEx15_morse부호.java
@@ -0,0 +1,25 @@
+package ch05_배열;
+class ArrayEx15_morse부호 {
+	public static void main(String[] args) {
+		
+		String source = "SOSHELP";
+		
+		String[] morse = {//a~z까지
+						   ".-", "-...", "-.-.","-..", "."
+						,"..-.", "--.", "....","..",".---"
+						, "-.-", ".-..", "--", "-.", "---"
+						, ".--.", "--.-",".-.","...","-"
+						, "..-", "...-", ".--", "-..-","-.--"
+						, "--.." 
+						};
+
+		String result="";
+
+		for (int i=0; i < source.length(); i++ ) {
+			result += morse[source.charAt(i)-'A'];
+		}
+		
+		System.out.println("source:"+ source);
+		System.out.println("morse:"+result);
+	}
+}
