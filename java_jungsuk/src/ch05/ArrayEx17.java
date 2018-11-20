f045a667570d41e7e90ee463ff23db71511ab61f
ytw9699 <ytw9699@gmail.com>
2018-11-20 21:30:59 +0900

    커맨드라인

--- java_jungsuk/src/ch05/ArrayEx17.java
+++ java_jungsuk/src/ch05/ArrayEx17.java
@@ -5,7 +5,6 @@ class ArrayEx17 {
 			System.out.println("usage: java ArrayEx15 NUM1 OP NUM2");
 			System.exit(0);
 		}
-
 		int num1 = Integer.parseInt(args[0]);   // 문자열을 숫자로 변환한다.
 		char op = args[1].charAt(0);            // 문자열을 문자(char)로 변환한다.
 		int num2 = Integer.parseInt(args[2]);
@@ -20,6 +19,7 @@ class ArrayEx17 {
 				result = num1 - num2;
 				break;
 			case 'x':
+				
 				result = num1 * num2;
 				break;
 			case '/':
@@ -28,7 +28,6 @@ class ArrayEx17 {
 			default :
 				System.out.println("지원되지 않는 연산입니다.");
 		}
-
 		System.out.println("결과:"+result);
 	}
 }
