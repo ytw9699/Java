01ef9c271c55b3770788b530d84032b3f73315bd
ytw9699 <ytw9699@gmail.com>
2018-11-20 15:45:28 +0900

    배열이용 총점과평균

--- java_jungsuk/src/ch05/ArrayEx05_총점과평균.java
+++ java_jungsuk/src/ch05/ArrayEx05_총점과평균.java
@@ -1,8 +1,10 @@
 package ch05;
-class ArrayEx05 {
+class ArrayEx05_총점과평균 {
 	public static void main(String[] args) {
+		
 		int sum =0;				// 총점을 저장하기 위한 변수
 		float average = 0f;		// 평균을 저장하기 위한 변수
+		System.out.println("평균 : " + average);
 
 		int[] score = {100, 88, 100, 100, 90};
 
