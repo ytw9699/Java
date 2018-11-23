2c2e737c3eea2089168695500f21e29ecf60c2f1
ytw9699 <ytw9699@gmail.com>
2018-11-23 17:21:47 +0900

    기본적 이터레이터사용

---
+++ iam_java/src/chap22/a02_IteratorUsage.java
@@ -0,0 +1,31 @@
+package chap22;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class a02_IteratorUsage
+{
+	public static void main(String[] args)
+	{
+		LinkedList<String> list=new LinkedList<String>();
+		list.add("First");
+		list.add("Second");
+		list.add("Third");
+		list.add("Fourth");
+		
+		Iterator<String> itr=list.iterator();
+		
+		System.out.println("반복자를 이용한 1차 출력과 \"Third\" 삭제");
+		while(itr.hasNext())
+		{
+			String curStr=itr.next();
+			System.out.println(curStr);
+			if(curStr.compareTo("Third")==0)//a와 b가 같으면 0을반환
+				itr.remove();
+		}
+		
+		System.out.println("\n\"Third\" 삭제 후 반복자를 이용한 2차 출력 ");		
+		itr=list.iterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+	}
+}
