58d2b95bc59ec29aa0a70fc4a8d84b3fc48e666c
ytw9699 <ytw9699@gmail.com>
2018-11-23 18:06:02 +0900

    해쉬셋의 중복여부 및 성능판단여부

---
+++ iam_java/src/chap22/a04_HashSetEqualityTwo.java
@@ -0,0 +1,45 @@
+package chap22;
+import java.util.Iterator;
+import java.util.HashSet;
+
+class SimpleNumber2
+{
+	int num;
+	public SimpleNumber2(int n)
+	{
+		num=n;
+	}
+	public String toString()
+	{
+		return String.valueOf(num);
+	}
+	public int hashCode()//해시코드는 성능을 개선해주는것이다 num 값을 3으로 나눠서 분류해둔다
+	{
+		return num%3;
+	}
+	public boolean equals(Object obj) // 이 메소드의 리턴값으로 해시셋의 중복여부를 결정한다
+	{
+		SimpleNumber2 comp=(SimpleNumber2)obj;
+		if(comp.num==num)
+			return true;
+		else
+			return false;		
+	}
+}
+
+class a04_HashSetEqualityTwo
+{
+	public static void main(String[] args)
+	{
+		HashSet<SimpleNumber2> hSet=new HashSet<SimpleNumber2>();
+		hSet.add(new SimpleNumber2(10));//이 add메소드어딘가에서 해시코드와 이콜스 메소드와 연관된 부분이 작동되는듯하다 그래서 성능과 중복여부를 판단하는것이다
+		hSet.add(new SimpleNumber2(20));
+		hSet.add(new SimpleNumber2(20));
+		
+		System.out.println("저장된 데이터 수: "+hSet.size());
+		
+		Iterator<SimpleNumber2> itr=hSet.iterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+	}
+}
