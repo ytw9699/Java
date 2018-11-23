7f0cf4579b8cdf1de360f4a7db7184d6afa189be
ytw9699 <ytw9699@gmail.com>
2018-11-23 19:57:12 +0900

    LinkedHashSet의 특성

--- java_jungsuk/src/ch11/Bingo.java
+++ java_jungsuk/src/ch11/Bingo.java
@@ -4,7 +4,7 @@ import java.util.*;
 class Bingo { 
       public static void main(String[] args) { 
             Set set = new HashSet(); 
-//          Set set = new LinkedHashSet(); 
+//          Set set = new LinkedHashSet(); //이걸쓰면 오름차순정렬안됨
             int[][] board = new int[5][5]; 
 
             for(int i=0; set.size() < 25; i++) { 
@@ -15,7 +15,7 @@ class Bingo {
 
             for(int i=0; i < board.length; i++) { 
                   for(int j=0; j < board[i].length; j++) { 
-                        board[i][j] = Integer.parseInt((String)it.next());
+                        board[i][j] = Integer.parseInt((String)it.next());//여기서 이터레이터가 오름차순정렬해줌 그래서 중간중간 오름차순으로나오는거
                         System.out.print((board[i][j] < 10 ? "  " : " ") + board[i][j]); 
                   } 
                   System.out.println(); 
