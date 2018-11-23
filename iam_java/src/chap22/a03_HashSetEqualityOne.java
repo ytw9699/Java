54295cf43c0871ed8f8c7ebc9e7659b2b1e13f27
ytw9699 <ytw9699@gmail.com>
2018-11-23 17:22:20 +0900

    해쉬셋의 특성

---
+++ iam_java/src/chap22/a03_HashSetEqualityOne.java
@@ -0,0 +1,34 @@
+package chap22;
+import java.util.Iterator;
+import java.util.HashSet;
+
+class SimpleNumber
+{
+	int num;
+	public SimpleNumber(int n)
+	{
+		num=n;
+	}
+	public String toString()
+	{
+		return String.valueOf(num);//int를 스트링으로
+	}
+}
+
+class a03_HashSetEqualityOne
+{
+	public static void main(String[] args)
+	{
+		HashSet<SimpleNumber> hSet=new HashSet<SimpleNumber>();
+		hSet.add(new SimpleNumber(10));
+		hSet.add(new SimpleNumber(20));
+		hSet.add(new SimpleNumber(20));//인스터스마다 다른주소값이니까 다른 걸로 인식해서 중복값이 출력
+		//같은값으로 인식하길 원한다면 내가 커스토마이징해야한다
+		
+		System.out.println("저장된 데이터 수: "+hSet.size());
+		
+		Iterator<SimpleNumber> itr=hSet.iterator();
+		while(itr.hasNext())
+			System.out.println(itr.next());
+	}
+}
