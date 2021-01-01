cb6f3ceb95f4767d8d702d5a742b0e5434935278
home_champ <ytw9699@naver.com>
2021-01-01 22:16:17 +0900

    상속과 생성자

--- src/JavaCodingEverybody1/k_상속과생성자3/c_상속과생성자0.java
+++ src/JavaCodingEverybody1/k_상속과생성자3/c_상속과생성자0.java
@@ -1,9 +1,15 @@
 ﻿package k_상속과생성자3;
+/*만약 매개변수를 가지는 생성자를 하나라도 정의했다면, 기본 생성자는 자동으로 추가되지 않는다.
+따라서 매개변수를 가지는 생성자를 하나 이상 정의한 후 기본 생성자를 호출하면, 오류가 발생한다
+그래서 수동적으로 기본 생성자를 만들어줘야한다.*/
+
 /*첫번째방법
 하위 클래스가 호출될 때 자동으로 상위 클래스의 기본 생성자를 호출하게 된다.
 그런데 상위 클래스에 매개변수가 있는 생성자가 있다면 자바는 자동으로 상위 클래스의 기본 생성자를 만들어주지 않는다. 
 따라서 존재하지 않는 생성자를 호출하게 되기 때문에 에러가 발생했다 그러므로 기본생성자를 만들어줘서 오류 해결*/
+
 class Calculator {
+	
    int left, right;
    /*  public void setOprands(int left, int right) {
    this.left = left;
@@ -13,11 +19,11 @@ class Calculator {
    	//밑에 매개변수 있는 생성자가 있기에 자동으로 기본생성자 안만들어준다//
   
    public Calculator(int left, int right){
-	   //새롭게 바뀐부분 left와 right를 받는 매개변수를받는 생성자 만들었기때문에
+	   //새롭게 바뀐부분 left와 right를 매개변수로 받는 생성자 만들었기때문에
 	   //new SubstractionableCalculator(10, 20);
 	   //이것을 만들때 상위클래스인 Calculator의 기본생성자를 호출하는데
 	   //기본생성자를 이 생성자때문에 자동으로 만들어주지않고
-	   //호출할수없어 오류가 발생하니 위와같이 기본생성자를 만들어줘야함!
+	   //호출할 수 없어 오류가 발생하니 위와같이 기본생성자를 만들어줘야함!
        this.left = left;
        this.right = right;
    }
@@ -28,17 +34,23 @@ class Calculator {
        System.out.println((this.left + this.right) / 2);
    }
 }
+
 class SubstractionableCalculator extends Calculator {
+	
    public SubstractionableCalculator(int left, int right) {
        this.left = left;
        this.right = right;
    }
+   
    public void substract() {
        System.out.println(this.left - this.right);
    }
 }
+
 public class c_상속과생성자0 {
+	
    public static void main(String[] args) {
+	   
        SubstractionableCalculator c1 = new SubstractionableCalculator(10, 20);
        c1.sum();
        c1.avg();
--- src/JavaCodingEverybody1/k_상속과생성자4/c_상속과생성자super.java
+++ src/JavaCodingEverybody1/k_상속과생성자4/c_상속과생성자super.java
@@ -1,46 +1,55 @@
 ﻿package k_상속과생성자4;
 
 class Calculator {
-   int left, right;
-   /* public void setOprands(int left, int right) {
-   this.left = left;
-   this.right = right;
-}*/
-    
-   //public Calculator(){}//이제는 없어도 상관없다//새롭게 바뀐부분 
-  
-   public Calculator(int left, int right){
-       this.left = left;
-       this.right = right;
-   }
- 
-   public void sum() {
-       System.out.println(this.left + this.right);
-   }
-   public void avg() {
-       System.out.println((this.left + this.right) / 2);
-   }
+	
+	   int left, right;
+	   /* public void setOprands(int left, int right) {
+	   this.left = left;
+	   this.right = right;
+		}*/
+	    
+	   //public Calculator(){}//이제는 없어도 상관없다//새롭게 바뀐부분 
+	  
+	   public Calculator(int left, int right){
+	       this.left = left;
+	       this.right = right;
+	   }
+	 
+	   public void sum() {
+	       System.out.println(this.left + this.right);
+	   }
+	   public void avg() {
+	       System.out.println((this.left + this.right) / 2);
+	   }
 }
+
 class SubstractionableCalculator extends Calculator {
-	public SubstractionableCalculator(int left, int right) {//2번째방법은
+	
+		public SubstractionableCalculator(int left, int right) {//2번째방법은
 		//이 생성자 로직이 위와 중복된다면 이걸 통해서 해결하는 2번째방법은 다음장 super 부모 클래스의 생성자를 의미
-		//super 키워드는 부모 클래스를 의미한다
-		// 여기에 ()붙이면 부모 클래스의 생성자를 의미하게 된다
-	    super(left , right);//부모클래스 생성자에게 여기서 매개변수로 left값과 right값을 받아서
-	    //public Calculator(int left, int right) 이쪽으로 전달하는것
-	    //그러면 굳이 부모클래스의 기본생성자 안만들어줘도 되자나 매개변수에 값이들어가면 기본생성자 안만들어줘도됨
-	    //그리고 하위클래스에 초기화작업을 하고싶으면 
-	    //먼저 부모클래스의 초기화작업을 먼저하고서 해야한다
-	}
-   public void substract() {
-       System.out.println(this.left - this.right);
-   }
+		
+		    super(left , right);
+		    //super 키워드는 부모 클래스를 의미한다
+		  	//여기에 ()붙이면 부모 클래스의 생성자를 의미하게 된다
+		    //부모클래스 생성자에게 여기서 매개변수로 left값과 right값을 받아서
+		    //public Calculator(int left, int right) 이쪽으로 전달하는것
+		    //그러면 굳이 부모클래스의 기본생성자 안만들어줘도 된다. 매개변수에 값이들어가면 기본생성자 안만들어줘도됨
+		    //그리고 하위클래스에 초기화작업을 하고싶으면 
+		    //먼저 부모클래스의 초기화작업을 먼저하고서 해야한다
+		}
+		
+	   public void substract() {
+	       System.out.println(this.left - this.right);
+	   }
 }
+
 public class c_상속과생성자super {
-   public static void main(String[] args) {
-       SubstractionableCalculator c1 = new SubstractionableCalculator(10, 20);
-       c1.sum();
-       c1.avg();
-       c1.substract();
-   }
+	
+	   public static void main(String[] args) {
+		   
+	       SubstractionableCalculator c1 = new SubstractionableCalculator(10, 20);
+	       c1.sum();
+	       c1.avg();
+	       c1.substract();
+	   }
 }
