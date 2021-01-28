e31a042d87d80b06170d5c791328fc8a3481cf3e
home_champ <ytw9699@naver.com>
2021-01-28 09:36:18 +0900

    스트림예제

--- src/JavaCodingEverybody1/Z_스트림/StreamEx.java
+++ src/JavaCodingEverybody1/Z_스트림/StreamEx.java
@@ -7,8 +7,37 @@ import java.util.stream.Stream;
 
 public class StreamEx{
 		
-		public void prevStream() {
+		static void prevStream(String[] strArr, List<String> strList) {
 			
+			 	for(String str : strArr) {
+		    		System.out.println(str);
+		    	}
+		    	
+		    	for(String str : strList) {
+		    		System.out.println(str);
+		    	}
+		    	
+		    	System.out.println();
+		    	
+		    	Arrays.sort(strArr);
+		    	Collections.sort(strList);
+		    	
+		    	for(String str : strArr) {
+		    		System.out.println(str);
+		    	}
+		    	
+		    	for(String str : strList) {
+		    		System.out.println(str);
+		    	}
+		}
+		
+		static void afterStream(String[] strArr, List<String> strList) {
+			
+			Stream<String> strStream1 = strList.stream(); // 스트림을 생성
+	    	Stream<String> strStream2 = Arrays.stream(strArr); // 스트림을 생성
+	    	
+	    	strStream1.forEach(System.out::println);
+	    	strStream2.forEach(System.out::println);
 		}
 	
 	    public static void main(String[] args) {
@@ -16,32 +45,7 @@ public class StreamEx{
 	    	String[] strArr = { "bbb", "aaa", "ccc"};
 	    	List<String> strList = Arrays.asList(strArr);
 	    	
-	    /*	for(String str : strArr) {
-	    		System.out.println(str);
-	    	}
-	    	
-	    	for(String str : strList) {
-	    		System.out.println(str);
-	    	}
-	    	
-	    	System.out.println();
-	    	
-	    	Arrays.sort(strArr);
-	    	Collections.sort(strList);
-	    	
-	    	for(String str : strArr) {
-	    		System.out.println(str);
-	    	}
-	    	
-	    	for(String str : strList) {
-	    		System.out.println(str);
-	    	}*/
-	    			
-	    	Stream<String> strStream1 = strList.stream(); // 스트림을 생성
-	    	Stream<String> strStream2 = Arrays.stream(strArr); // 스트림을 생성
-	    	
-	    	strStream1.sorted().forEach(System.out::println);
-	    	strStream2.sorted().forEach(System.out::println);
-	    	
+	    	prevStream(strArr, strList);
+	    	afterStream(strArr, strList);
 	    }
 }
